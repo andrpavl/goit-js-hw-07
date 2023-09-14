a94857030b7a6e64573fe060285281a23041472f
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-09-14 09:41:51 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 
 const container = document.querySelector(".gallery");
 
-const markup = galleryItems.map(
-  ({ preview, original, description }) =>
-    `<li class="gallery__item">
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) =>
+			`<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img 
     class="gallery__image" 
@@ -15,31 +15,30 @@ const markup = galleryItems.map(
     data-source="${original}">
     </a>
     </li>`
-).join("");
+	)
+	.join("");
 container.insertAdjacentHTML("beforeend", markup);
-container.addEventListener('click', onClick)
-let instance
+container.addEventListener("click", onClick);
+let instance;
 function onClick(evt) {
-    evt.preventDefault();
-    if (!evt.target.classList.contains('gallery__image')) {
-        return
-    }
-    const originalImage = evt.target.dataset.source;
-    const originalImageAlt = evt.target.alt
-    instance = basicLightbox.create(`
+	evt.preventDefault();
+	if (!evt.target.classList.contains("gallery__image")) {
+		return;
+	}
+	const originalImage = evt.target.dataset.source;
+	const originalImageAlt = evt.target.alt;
+	instance = basicLightbox.create(`
     <div class="modal">
         <img src="${originalImage}" alt="${originalImageAlt}">
     </div>
     `);
-    instance.show();
+	instance.show();
 }
 
-document.addEventListener('keydown', evt => {
-    if (evt.code === 'Escape') {
-      if(instance){
-      instance.close();
-      }
-    }
-  });
-
-
+document.addEventListener("keydown", (evt) => {
+	if (evt.code === "Escape") {
+		if (instance) {
+			instance.close();
+		}
+	}
+});
